1d1352536a05d05a0a5af55a2661851bec5d2dbb
SquareDot3301 <eragon941@outlook.fr>
2025-09-26 21:19:34 +0200

    Add allowed hosts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "test.quetzer.land",
   integrations: [react()],
 
+  server: { allowedHosts: ["test.quetzer.land"] },
+
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
