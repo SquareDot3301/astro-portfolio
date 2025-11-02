b4b2ecf55a8da1d87f1ab53ef268a8e19ffd788d
SquareDot3301 <eragon941@outlook.fr>
2025-11-02 10:16:17 +0100

    Modify matomo

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,10 +13,31 @@ import robots from "astro-robots";
 
 import metaTags from "astro-meta-tags";
 
+import matomo from "astro-matomo";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://richalguemei.fr",
-  integrations: [react(), sitemap(), robots(), metaTags()],
+  integrations: [
+    react(),
+    sitemap(),
+    robots(),
+    metaTags(),
+    matomo({
+      enabled: import.meta.env.PROD,
+      host: "https://richalguemei.fr:8080",
+      setCookieDomain: "*.richalguemei.fr",
+      trackerUrl: "/matomo.php", // Matomo sur ton port
+      srcUrl: "/matomo.js", // Matomo sur ton port
+      siteId: 1, // correspond à ton installation Matomo
+      heartBeatTimer: 5,
+      disableCookies: true,
+      debug: false,
+      viewTransition: {
+        contentElement: "main",
+      },
+    }),
+  ],
   server: { allowedHosts: ["richalguemei.fr"] },
 
   vite: {
